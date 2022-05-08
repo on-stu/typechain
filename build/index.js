6b2490972fb3c90244052bf691ab1482ef97ab53
on-stu <minsu0523@naver.com>
2022-05-08 14:26:30 +0900

    finish

--- build/index.js
+++ build/index.js
@@ -1,1 +1,11 @@
-const hello = () => "hello";
+import { exit, init } from "myPackage";
+class Block {
+    constructor(data) {
+        this.data = data;
+    }
+    static hello() {
+        return "hi";
+    }
+}
+init({ urls: "hello" });
+exit(1);
